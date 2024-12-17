13fd57fa042a23dae79d9cd698d9dc8bf7bae457
zujeily <20233tn151@utez.edu.mx>
2024-12-17 01:09:09 -0600

    Ramas creadas

---
+++ src/main/java/org/example/supermecado/SupermecadoApplication.java
@@ -0,0 +1,13 @@
+package org.example.supermecado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermecadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermecadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/supermecado/SupermecadoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.supermecado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermecadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
